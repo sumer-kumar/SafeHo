b51435abfba3fb50b03a7cd9f709f2cd857dbcf1
Sumer Kumar <sumer47kumar@gmail.com>
2021-04-09 22:40:32 +0530

    added resourse files

---
+++ app/src/main/java/com/sumer/safeho/LoginActivity1.java
@@ -0,0 +1,16 @@
+package com.sumer.safeho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sumer.safeho.R;
+
+public class LoginActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+    }
+}
